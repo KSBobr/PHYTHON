11eed177dc4a45f1747fd59b39643a8662d33c78
KSBobr <143553245+KSBobr@users.noreply.github.com>
2024-09-23 01:18:17 +0300

    Create task4.py

---
+++ PythonCourceProject/ ├── 1week/task4.py
@@ -0,0 +1,46 @@
+"""
+https://leetcode.com/problem-list/string/
+url: https://leetcode.com/problems/string-to-integer-atoi/?envType=problem-list-v2&envId=string
+"""
+
+class Solution(object):
+    def myAtoi(self, s):
+        n=len(s)
+        ans=0
+        zn=1
+        zni=0
+        znt=0
+        startint=0
+        endint=0
+        if (s==""): return (ans)
+        Ch="0123456789"
+        for i in range (n):
+            if (s[i] in Ch):
+                startint=i
+                break
+            else:
+                if(not((s[i]==' ') or s[i]=='-' or s[i]=='+')):
+                    return ans
+                elif (s[i]=='-' and znt==0): 
+                    zn=zn*-1
+                    zni=i
+                    znt=1
+                elif (s[i]=='+' and znt==0): 
+                    zni=i
+                    znt=1
+        if(znt and zni+1!=startint):return(ans)                                      
+        if (startint==0  and not(s[0] in Ch)):return (ans)          
+        for i in range (startint,n):
+            if(not(s[i]in Ch)):
+                endint=i
+                break    
+        
+        if (endint==0): 
+            endint=n
+        ch=s[startint:endint]
+        print(startint,endint)
+        print(ch)
+        ans=int(ch)*zn
+        if(ans<-2**31):ans=-2**31
+        if(ans>2**31-1): ans=2**31-1
+        return (ans)
