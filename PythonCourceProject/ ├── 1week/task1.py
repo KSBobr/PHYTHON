763c7e6c813b34fd4cad305a1c160396c52e10d0
KSBobr <143553245+KSBobr@users.noreply.github.com>
2024-09-23 01:08:42 +0300

    Create task1.py

---
+++ PythonCourceProject/ ├── 1week/task1.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def countAndSay(self, n):
+        def NS(s):
+            n=len(s)
+            s=s
+            c=1
+            news=''
+            if(len(s)==1):
+                news='1'+s
+                return (news)
+            else:
+                s+='a'
+                for i in range(1,n+1):
+                    if(s[i-1]==s[i]):
+                        c+=1
+                    else: 
+                        news+=str(c)+str(s[i-1])
+                        c=1
+                return news
+        st="1"
+        for i in range(1,n):
+            st=NS(st)
+            print(st)
+        return st
